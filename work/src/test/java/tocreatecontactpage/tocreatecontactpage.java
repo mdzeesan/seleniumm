383a0f6bcb86e4c8b95543fd028728cd2319f0d4
Admin <Admin@DESKTOP-4SV1OS7>
2024-11-06 08:38:40 +0530

    push

---
+++ work/src/test/java/tocreatecontactpage/tocreatecontactpage.java
@@ -0,0 +1,8 @@
+package tocreatecontactpage;
+
+public class tocreatecontactpage {
+	public static void main(String[] args) {
+		System.out.println("this is to create contact page");
+	}
+
+}
---
+++ work/src/test/java/tocreateorgpage/tocreateoranizationpage.java
@@ -0,0 +1,8 @@
+package tocreateorgpage;
+
+public class tocreateoranizationpage {
+	public static void main(String[] args) {
+		System.out.println("this is to create organization page");
+	}
+
+}
